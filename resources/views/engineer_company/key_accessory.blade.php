5a03daf8c40206f2f19e774d335da94c26756a2a
JerryPrograms <zainaftab675@gmail.com>
2023-01-30 11:08:54 +0500

    responsiveness

--- resources/views/engineer_company/key_accessory.blade.php
+++ resources/views/engineer_company/key_accessory.blade.php
@@ -8,15 +8,8 @@
                 <!-- start page title -->
                 <div class="row">
                     <div class="col-12">
-                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <div class="page-title-box d-sm-flex align-items-center justify-content-between ms-2">
                             <h4 class="mb-sm-0 font-size-18">File Manager</h4>
-
-                            <div class="page-title-right">
-                                <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Apps</a></li>
-                                    <li class="breadcrumb-item active">File Manager</li>
-                                </ol>
-                            </div>
 
                         </div>
                     </div>
