15f1a0af48d23faf2e72faddbdd7acb0298cda29
Iamqasimmansoor <qasimmansoor683@gmail.com>
2023-02-21 15:48:10 +0500

    admin issue fixed

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\admin\BuildingInfoManagementController;
+use App\Http\Controllers\Admin\BuildingInfoManagementController;
 use App\Http\Controllers\Admin\AdminController;
 
 /*
