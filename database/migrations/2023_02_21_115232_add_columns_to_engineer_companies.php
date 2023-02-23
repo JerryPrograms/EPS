daed198b52f5bf0a74d99179a89c1336f9559a7c
JerryPrograms <zainaftab675@gmail.com>
2023-02-23 11:23:20 +0500

    update migrations

--- database/migrations/2023_02_21_115232_add_columns_to_engineer_companies.php
+++ database/migrations/2023_02_21_115232_add_columns_to_engineer_companies.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,9 +13,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('engineer_companies', function (Blueprint $table) {
-            $table->longText('address')->nullable()->default('address...');
-            $table->string('manager_name',100)->nullable()->default('manager name...');
-            $table->string('contract_number',100)->nullable()->default('00-0000-0000');
+            $table->longText('address')->nullable();
+            $table->string('manager_name', 100)->nullable()->default('manager name...');
+            $table->string('contract_number', 100)->nullable()->default('00-0000-0000');
         });
     }
 
@@ -28,7 +27,9 @@ return new class extends Migration
     public function down()
     {
         Schema::table('engineer_companies', function (Blueprint $table) {
-            //
+            $table->dropColumn('address');
+            $table->dropColumn('manager_name', 100);
+            $table->dropColumn('contract_number', 100);
         });
     }
 };
