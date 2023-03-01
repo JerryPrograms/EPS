4a9fb8181471d0144ca052f14ebb417190d330fe
JerryPrograms <zainaftab675@gmail.com>
2023-03-01 16:34:53 +0500

    Update EngineerCompanyController.php

--- app/Http/Controllers/engineercompany/EngineerCompanyController.php
+++ app/Http/Controllers/engineercompany/EngineerCompanyController.php
@@ -41,10 +41,12 @@ class EngineerCompanyController extends Controller
             $customer = CustomerInfo::orWhere(function ($query) use ($engineers) {
                 $query->where('added_by', 'engineer')
                     ->where('added_by_id', $engineers->id);
-            })->orWhere(function ($query) {
-                $query->where('added_by', activeGuard())
-                    ->where('added_by_id', auth(activeGuard())->id());
-            })->latest()->paginate(10);
+            })
+//                ->orWhere(function ($query) {
+//                $query->where('added_by', activeGuard())
+//                    ->where('added_by_id', auth(activeGuard())->id());
+//            })
+                ->latest()->paginate(10);
         }
         return view('engineer_company.customer_list', compact('customer'));
     }
