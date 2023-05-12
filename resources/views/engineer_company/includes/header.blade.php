688768fc61dfe0e7c93f315222f0f985b52a6926
JerryPrograms <zainaftab675@gmail.com>
2023-05-12 18:21:42 +0500

    Update header.blade.php

--- resources/views/engineer_company/includes/header.blade.php
+++ resources/views/engineer_company/includes/header.blade.php
@@ -9,8 +9,13 @@
                 <div style="text-align: right;display: flex;" class="profile_section">
                     <img src="{{asset('engineer_company/assets/images/Vector(2).png')}}" class="user_img mr-4">
                     <div class="dropdown">
-                        <p id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"
-                           class="mb-sm-0 font-size-18 user_test dropdown-toggle">{{auth(activeGuard())->user()->name}}</p>
+                        @if(activeGuard() == 'admin')
+                            <p id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"
+                               class="mb-sm-0 font-size-18 user_test dropdown-toggle">{{auth(activeGuard())->user()->name}}</p>
+                        @else
+                            <p id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"
+                               class="mb-sm-0 font-size-18 user_test dropdown-toggle">{{auth(activeGuard())->user()->company_name}}</p>
+                        @endif
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item" href="{{route('ec.EngineerCompanyLogout',activeGuard())}}"><i
                                     class="text-danger fa fa-power-off me-2"></i>{{ __('translation.logout') }}</a>
