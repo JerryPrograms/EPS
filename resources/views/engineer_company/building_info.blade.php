bb35c68faf3ee46a1c05aca99723f87027f94801
Iamqasimmansoor <qasimmansoor683@gmail.com>
2023-07-19 17:00:15 +0500

    Tuesday_05:00pm_19_july_2023 code (merged)
    Tuesday_05:00pm_19_july_2023 code (merged)

--- resources/views/engineer_company/building_info.blade.php
+++ resources/views/engineer_company/building_info.blade.php
@@ -330,7 +330,7 @@
                                                         </div>
 
 
-                                                        <div class="row align-items-center mt-4">
+                                                        <div class="row align-items-center">
                                                             <div class="col-md-4 col-12">
                                                                 <label class="form-label custom_lab mb-0"> <span
                                                                         class="star_section">*</span>
