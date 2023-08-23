a532fe81c90613a9c2e5e28bb21ad016d93bec95
Iamqasimmansoor <qasimmansoor683@gmail.com>
2023-08-23 16:19:18 +0500

    resolve 3 (merged)
    resolve 3 (merged)

--- resources/views/engineer_company/construction_completion.blade.php
+++ resources/views/engineer_company/construction_completion.blade.php
@@ -73,7 +73,6 @@
 
                                             @if(count($completion_reports) > 0)
                                                 @foreach($completion_reports as $c)
-                                                    @dd($c->GetCustomer)
                                                     <tr>
                                                         <td>
                                                             {{$loop->index + 1}}
